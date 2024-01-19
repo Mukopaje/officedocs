2e787fb9ad20a7c76947148dc05e75368deaa414
Mukopaje Singogo <mukopaje@digitalsystems.biz>
2024-01-19 12:02:56 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = u'Workice CRM'
+project = u'CRM'
 copyright = u'2019, William Mandai'
 author = u'William Mandai'
 
@@ -108,7 +108,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'WorkiceDocsdoc'
+htmlhelp_basename = 'CRMDocsdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -135,7 +135,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'WorkiceDocs.tex', u'Workice CRM Documentation',
+    (master_doc, 'WorkiceDocs.tex', u'CRM Documentation',
      u'William Mandai', 'manual'),
 ]
 
@@ -145,7 +145,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'workicedocs', u'Workice CRM Documentation',
+    (master_doc, 'crmdocs', u'CRM Documentation',
      [author], 1)
 ]
 
@@ -156,8 +156,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'WorkiceCRM', u'Workice CRM Documentation',
-     author, 'WorkiceCRM', 'One CRM to manage your entire invoicing, expenses, deals and projects',
+    (master_doc, 'CRM', u'CRM Documentation',
+     author, 'CRM', 'One CRM to manage your entire invoicing, expenses, deals and projects',
      'Miscellaneous'),
 ]
 
